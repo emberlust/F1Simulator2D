fc9eb59cec7b1eebf2e961c4fe84be19df4475a9
Emberlust <albuica11@gmail.com>
2022-04-20 17:27:33 +0300

    Added Facade

--- src/F1Application.java
+++ src/F1Application.java
@@ -1,52 +1,15 @@
-import car.CarFactory;
-import data.DataHandler;
-import data.Pilot;
-import data.Race;
-import data.ScoreBoard;
-import map.Map;
-import map.MapBuilder;
-import gui.Window;
-import car.Car;
+import data.RaceFacade;
 
 public class F1Application {
 
 	public static void main(String[] args) {
 		
-		ScoreBoard score = null;
+		RaceFacade f1 = new RaceFacade();
 		
-		
-		CarFactory cf = new CarFactory();
-		
-		Pilot[] pilots = new Pilot[4];
-		
-		pilots[0] = new Pilot("gigel",new Car(1,10,0,999,1),"Mercedes");
-		pilots[1] = new Pilot("ionica",new Car(2,5,0,999,1),"Redbull");
-		pilots[2] = new Pilot("gheorge",new Car(3,6,0,999,1),"TurboP");
-		pilots[3] = new Pilot("costica", cf.get_car(150),"Mercedes");
-		
-		Map race_map = new MapBuilder()
-				.set_size(20)
-				.init(true)
-				.generate(true)
-				.build_map();
-		
-		Race f1 = new Race(race_map,1);
-		f1.place_participant(pilots[0]);
-		f1.place_participant(pilots[1]);
-		f1.place_participant(pilots[2]);
-		f1.place_participant(pilots[3]);
-		
-	   Window window = new Window(f1);
-		
-	    
-
-		//f1.start_race();
-		
-		score = f1.get_score();
-		
-		
-		//score = DataHandler.pull_data();
-		//DataHandler.push_data(score);
+		f1.choose_pilots();
+		f1.chose_map_and_loops();
+		f1.prepare_race();
+		f1.simulate_race();
 	}
 
 }
---
+++ src/data/RaceFacade.java
@@ -0,0 +1,63 @@
+package data;
+
+import car.Car;
+import car.CarFactory;
+import gui.Window;
+import map.Map;
+import map.MapBuilder;
+
+public class RaceFacade {
+
+	private Race race;
+	private Map map;
+	private int nof_loops;
+	private Pilot[] pilots;
+	
+	public RaceFacade()
+	{
+		
+	}
+	
+	public void choose_pilots()
+	{
+		CarFactory cf = new CarFactory();
+		
+		this.pilots = new Pilot[4];
+		pilots[0] = new Pilot("gigel",new Car(1,10,0,999,1),"Mercedes");
+		pilots[1] = new Pilot("ionica",new Car(2,5,0,999,1),"Redbull");
+		pilots[2] = new Pilot("gheorge",new Car(3,6,0,999,1),"TurboP");
+		pilots[3] = new Pilot("costica", cf.get_car(150),"Mercedes");
+	}
+	
+	public void chose_map_and_loops()
+	{
+		int size = 20;
+		map = new MapBuilder()
+				.set_size(size)
+				.init(true)
+				.generate(true)
+				.build_map();
+		this.nof_loops = 1;
+	}
+	
+	public void prepare_race()
+	{	
+		
+		race = new Race(this.map, nof_loops);
+		
+		for(Pilot pilot : pilots)
+		{
+			race.place_participant(pilot);
+		}
+		
+		Window window = new Window(this.race);
+		
+	}
+	
+	public ScoreBoard simulate_race()
+	{
+		this.race.start_race();
+		return this.race.get_score();
+	}
+	
+}
